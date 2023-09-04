7dc9201aa765e23c5d4c485031fef5aefa551f7d
Im-Derrick <142809926+Im-Derrick@users.noreply.github.com>
2023-09-04 13:25:33 +0200

    Add files via upload

---
+++ triangel.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=1;
+    
+    for (int i; i++; i<=7);{
+        printf("*\n");
+
+         do{
+            printf("*");
+
+            i++;
+
+            
+        } while (i<7);
+      
+    }
+    
+
+    printf("\n");
+
+    return 0;
+    
+}
